2ce7f3ea9ddbd475d07cccfebc37634599bb5f62
Bernardo Rios <bernardo_palmeira@hotmail.com>
2022-07-14 17:38:02 -0300

    fazendo a 1

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,10 +1,49 @@
 import React from 'react';
 
 function Login() {
+  // enableButton = () => {
+  //   const { inputName, inputEmail } = useState();
+  //   const minCharacters = 1;
+  //   const regex = /\S+@\S+\.\S+/;
+  //   if (inputName.length >= minCharacters
+  //     && regex.test(inputEmail)) {
+  //     setState({ isEnabled: true });
+  //   } else {
+  //     setState({ isEnabled: false });
+  //   }
+  // };
   return (
-    <p>
-      Login
-    </p>
+    <div>
+      <label htmlFor="email">
+        <input
+          type="email"
+          id="email"
+          data-testid="email-input"
+          onChange={ handleChangeEmail }
+          value={ email }
+        />
+      </label>
+      <br />
+      <label htmlFor="senha">
+        <input
+          type="password"
+          id="senha"
+          data-testid="password-input"
+          // minLength="6"
+          // onChange={ this.setInput }
+          // value={ senha }
+        />
+      </label>
+      <br />
+      <button
+        type="button"
+        data-testid="login-submit-btn"
+        disabled={ buttonDisabled }
+        // onClick={ this.handleClick }
+      >
+        Entrar
+      </button>
+    </div>
   );
 }
 
